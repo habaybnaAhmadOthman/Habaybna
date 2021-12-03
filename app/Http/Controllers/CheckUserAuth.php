6980c0b3e344938259e5698c12b4626b7171b97b
ahmad.obeidat <ahmad.obeidat@olahub.com>
2021-12-04 00:52:27 +0200

    final

--- app/Http/Controllers/CheckUserAuth.php
+++ app/Http/Controllers/CheckUserAuth.php
@@ -20,26 +20,32 @@ class CheckUserAuth extends Controller
      */
     public function __invoke(Request $request)
     {
-        dd('xxxx');
-        if (Auth::check()) {
-            $user = Auth::user();
-            $userData['avatar']= 'avatar';
-            $userData['firstName']= $user->user_data->firsName;
-            $userData['lasttName']= $user->user_data->lastName;
-            $userData['type']= $user->role;
-            return response()->json([
-                'status'=>true,
-                'msg'=>'success',
-                'userData'=>$userData
-            ]);
-        }
+
         try{
+            if (Auth::check()) {
+
+                $user = Auth::user();
+                $userData['avatar']= 'avatar';
+                $userData['firstName']= $user->user_data->firstName;
+                $userData['lastName']= $user->user_data->lastName;
+                $userData['type']= $user->role;
+                return response()->json([
+                    'status'=>true,
+                    'msg'=>'success',
+                    'userData'=>$userData
+                ]);
+
+            }
+            return response()->json([
+                'msg'=>'faild',
+                'status'=>false,
+           ],404);
+
     } catch (ModelNotFoundException $e){
         return response()->json([
             'msg'=>'faild',
             'status'=>false,
-            404
-       ]);
+        ],404);
     }
 
 
