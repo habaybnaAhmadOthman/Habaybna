20ec1f86ec1099dd175320b325afd4b609b22783
ahmad.obeidat <ahmad.obeidat@olahub.com>
2022-03-06 02:14:36 +0300

    remove passport

--- app/User.php
+++ app/User.php
@@ -5,11 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-// use Spatie\Permission\Traits\HasRoles;
-// use Laravel\Passport\HasApiTokens;
 use Laravel\Sanctum\HasApiTokens;
-
-use Laravel\Passport\PersonalAccessTokenResult;
 use App\ParentUsers;
 use App\Other;
 use App\Specialist;
