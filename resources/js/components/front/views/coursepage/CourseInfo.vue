aed0316a11e1565d28a6c568b3dcc4119c2834a7
montser <montaser.zalloum@arabiaweather.com>
2022-04-05 17:38:46 +0300

    fixes

--- resources/js/components/front/views/coursepage/CourseInfo.vue
+++ resources/js/components/front/views/coursepage/CourseInfo.vue
@@ -3,7 +3,7 @@
         <CourseInfoText :header="'نظرة عامة على الدورة'" :description="description"></CourseInfoText>
         <div class="mb-40 what-should-learn mt-20 mb-20-p">
             <h3 class="title-line font-27 mb-40 mb-20-p">ماذا سيتعلم المشاركون</h3>
-            <div class="font-27 black-2 font-20-p" v-html="whatShouldLearn"></div>
+            <div class="font-27 black-2 font-20-p li-auto" v-html="whatShouldLearn"></div>
         </div>
     </div>
 </template>
@@ -48,4 +48,5 @@ export default {
 /* .course-lectures .btn {
     padding:8px;
 } */
+
 </style>
--- resources/js/components/front/views/coursepage/CourseInfoText.vue
+++ resources/js/components/front/views/coursepage/CourseInfoText.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="banner-info mb-40 mb-20-p">
         <h2 class="title-line font-27 mb-40 do">{{header}}</h2>
-        <div class="black-2 font-20 pre-line f-normal" v-html="description"></div>
+        <div class="black-2 font-20 pre-line f-normal li-auto" v-html="description"></div>
     </div>
 </template>
 
@@ -11,11 +11,16 @@
         
     }
 </script>
-
+<style>
+.li-auto li{
+    list-style: inherit;
+}
+</style>
 <style scoped>
 .banner-info {
     margin-top: 150px;
 }
+
 @media (max-width: 767px) {
     .banner-info {
         margin-top: 20px;
