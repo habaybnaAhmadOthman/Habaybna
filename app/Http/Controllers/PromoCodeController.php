5a72e00388e9cd7c2ea6c3c1db04ff16854c2f60
ahmad.obeidat <ahmad.obeidat@olahub.com>
2022-05-16 14:47:27 +0300

    fix image 2

--- app/Http/Controllers/PromoCodeController.php
+++ app/Http/Controllers/PromoCodeController.php
@@ -9,7 +9,6 @@ use App\CustomClass\CheckPromoCode;
 use App\CustomClass\editCoupon;
 use App\CustomClass\AssignToUsers;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use App\Content;
 
 
 
@@ -47,18 +46,6 @@ class PromoCodeController extends Controller
     }
     public function show($id)
     {
-    $contents = Content::all();
-
-                foreach($contents as $content){
-        if($content->image){
-            $rest = substr($content->image, 39);  // returns "abcde"
-
-            $newUrl = "https://habaybna.net/storage/media/".$rest;
-
-            $content->image = $newUrl ;
-            $content->save();
-        }
-    }
         try{
             $coupons = PromoCode::findorfail($id);
             $data = [];
