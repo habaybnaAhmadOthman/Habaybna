411f902dbd42ad0ddf3d3c07c3b2daef8db2ba2b
montser <montaser.zalloum@arabiaweather.com>
2022-05-19 18:29:07 +0300

    gtm code

--- resources/js/router.js
+++ resources/js/router.js
@@ -496,7 +496,7 @@ router.beforeEach((to, from, next) => {
 });
 
 Vue.use(VueGtm, {
-    id: 'GTM-TS8LK93', // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
+    id: 'GTM-NZQPBCZ', // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
     queryParams: {
       // Add URL query string when loading gtm.js with GTM ID (required when using custom environments)
       gtm_auth: 'AB7cDEf3GHIjkl-MnOP8qr',
