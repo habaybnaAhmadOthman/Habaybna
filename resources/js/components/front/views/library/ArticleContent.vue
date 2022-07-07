c50c63d34b8e546261813a20b74306ee0637ba9a
montser <montaser.zalloum@arabiaweather.com>
2022-07-08 00:22:01 +0300

    save

--- resources/js/components/front/views/library/ArticleContent.vue
+++ resources/js/components/front/views/library/ArticleContent.vue
@@ -3,11 +3,11 @@
     <div class="box mt-30 flex-wrap-p p-side-12-p">
       <div class="content w-100-p">
         <h1 class="font-40 bold black font-28-p">{{ title }}</h1>
-        <router-link :to="`/specialist/${author.user_id}`" class="author-box bold mb-20 mt-10 d-flex align-center">
+        <router-link :to="authorURL" class="author-box bold mb-20 mt-10 d-flex align-center">
           <img :src="author.avatar | defaultAvatar" class="object-fit rounded" width="40" height="40">
           <div class="mr-10">
-            <p class="font-14 main-color underline"> بواسطة : {{author.firstName + ' ' + author.lastName}} </p>
-            <p class="font-14 gray">{{author.specialization}}</p>
+            <p class="font-14 main-color trans"> بواسطة : {{author.firstName + ' ' + author.lastName}} </p>
+            <p class="font-14 gray trans">{{author.specialization}}</p>
           </div>
         </router-link>
         <div
@@ -42,6 +42,16 @@ export default {
   components: {
     Tag,
   },
+  computed: {
+    authorURL(){
+      var url = this.author.firstName.split(' ').join('-')
+      if (this.author.lastName)
+        url += `-${this.author.lastName.split(' ').join('-')}`
+
+      url += `--${this.author.user_id}`
+      return `/specialist/${url}`
+    },
+  },
   filters: {
       defaultAvatar: function (avatar) {
           if (avatar == 'default.jpg') return '/images/avatars/default.jpg'
@@ -58,6 +68,9 @@ export default {
 .floating-ads {
   position: sticky;
   top: 10px;
+}
+.author-box:hover * {
+  color: #FF9E00;
 }
 @media (max-width: 767px) {
 }
