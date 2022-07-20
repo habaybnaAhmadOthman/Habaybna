5377e871ba0e821c429a304890873fb02fd1a03a
montser <montaser.zalloum@arabiaweather.com>
2022-07-20 16:18:09 +0300

    --

--- resources/js/components/front/views/specialists/HorizontalCard.vue
+++ resources/js/components/front/views/specialists/HorizontalCard.vue
@@ -1,5 +1,6 @@
 <template>
     <router-link to="/specialist/1" class="bg-white shadow radius-12 pt-10 pb-10 p-side-15 d-flex space-between">
+    <!-- <router-link :to="authorURL" class="bg-white shadow radius-12 pt-10 pb-10 p-side-15 d-flex space-between"> -->
         <div class="black-2">
             <p class="font-20 bold black-2 mb-15">د.محمد الأحمد</p>
             <p class="font-15 gray">أخصائي توحد</p>
@@ -10,7 +11,16 @@
 
 <script>
     export default {
-        
+        computed: {
+            authorURL(){
+                var url = this.author.firstName.split(' ').join('-')
+                if (this.author.lastName)
+                    url += `-${this.author.lastName.split(' ').join('-')}`
+
+                url += `--${this.author.user_id}`
+                return `/specialist/${url}`
+            },
+        }
     }
 </script>
 
