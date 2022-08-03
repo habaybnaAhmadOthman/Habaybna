28b831e7b7e520b7495f8a8391754c5e1b879a25
ahmad.obeidat <ahmad.obeidat@olahub.com>
2022-08-03 15:18:32 +0300

    fix bugs in appointments

--- app/Http/Controllers/CallsAppointmentsController.php
+++ app/Http/Controllers/CallsAppointmentsController.php
@@ -16,21 +16,21 @@ class CallsAppointmentsController extends Controller
 
     public function setCallsProvidersappointments(Request $request)
     {
-        // if(Auth::user()->specialist && Auth::user()->specialist->make_calls){
-        //     $data['apointments'] = $request->all();
+        if(Auth::user()->specialist && Auth::user()->specialist->make_calls){
+            $data['apointments'] = $request->all();
 
-        //     if(!empty($data['apointments']) && count($data['apointments']) > 0 ){
+            if(!empty($data['apointments']) && count($data['apointments']) > 0 ){
 
-        //         foreach ($data['apointments'] as $one) {
-        //             CallsAppointments::create([
-        //                 'specialist_id'=>Auth::id(),
-        //                 'appointment'=>$one
-        //             ]);
-        //         }
-        //         return response(Auth::user()->specialist->callsAppointments, 200);
-        //     }
-        // }
-        // return response('cantmakecall', 404);
+                foreach ($data['apointments'] as $one) {
+                    CallsAppointments::create([
+                        'specialist_id'=>Auth::id(),
+                        'appointment'=>$one
+                    ]);
+                }
+                return response(Auth::user()->specialist->callsAppointments, 200);
+            }
+        }
+        return response('cantmakecall', 404);
 
 
     }
@@ -48,8 +48,8 @@ class CallsAppointmentsController extends Controller
 
     public function getCallsProvidersappointments($slug)
     {
+        // dd($slug);
        $id = explode ("--", $slug);
-
         $provider = User::findorfail($id[1]) ;
         if($provider &&
         $provider->specialist &&
