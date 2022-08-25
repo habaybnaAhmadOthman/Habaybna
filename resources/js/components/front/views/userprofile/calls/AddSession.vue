54b95931cba817c72bf8456b431f15ede8443b5d
montser <montaser.zalloum@arabiaweather.com>
2022-08-26 00:41:56 +0300

    --

--- resources/js/components/front/views/userprofile/calls/AddSession.vue
+++ resources/js/components/front/views/userprofile/calls/AddSession.vue
@@ -62,8 +62,8 @@
       </form>
       <div class="d-flex gap-20 mt-10-p btns-box">
         <button @click="clear" class="radius-60 gray dismiss-btn pointer flex-1">إلغاء</button>
-        <button @click="submitForm(true)" class="radius-60 main-bg border-0 white font-18 pointer pt-15 pb-15 flex-1">حفظ</button>
-        <button @click="submitForm(false)" class="radius-60 main-bg border-0 white font-18 pointer pt-15 pb-15 flex-1">حفظ وإستمرار</button>
+        <button @click="submitForm(true)" class="radius-60 main-bg border-0 white font-18 pointer pt-15 pb-15 flex-1">إضافة</button>
+        <button @click="submitForm(false)" class="radius-60 main-bg border-0 white font-18 pointer pt-15 pb-15 flex-1">إضافة موعد جديد</button>
       </div>
     </div>
   </normal-modal>
