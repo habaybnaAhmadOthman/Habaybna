4e71c262267896b1a670f3b84f1e98ed553cb048
ahmad.obeidat <ahmad.obeidat@olahub.com>
2022-08-29 17:22:01 +0300

    js

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
- | Mix Asset Management     
+ | Mix Asset Management
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+    // if (mix.inProduction()) {
+    //     mix.version();
+    // }
+        mix.version();
