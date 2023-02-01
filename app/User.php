d77010d80bcdc6bda90af8f5cd68060e7e1c3f23
ahmad.obeidat <ahmad.obeidat@olahub.com>
2023-02-02 01:04:21 +0300

    users country & fix bugs in zoom account & mailerlite popup

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'phone', 'email','phone','otp','is_verify','role','password'
+        'phone', 'email','phone','otp','is_verify','role','password','country'
     ];
     protected $appends = ['user_data'];
 
